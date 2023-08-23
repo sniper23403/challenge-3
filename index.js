2e3d1a7c244a4eb3fc1a07b8a7e8003b264cd9fe
sniper23403 <100159541+sniper23403@users.noreply.github.com>
2023-08-23 15:56:48 +0100

    updating cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 
 const express = require("express");
 const path = require("path");
-const cors = require("cors");
+
 
 
 
@@ -17,6 +17,7 @@ const cors = require("cors");
  */
 
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || "8000";
 
 
@@ -28,10 +29,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use( 
-  cors({ 
-    origin: "https://sniper23403.github.io/challenge-3/",
-}))
+app.use(cors());
 
 
 
