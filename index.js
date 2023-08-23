3fffb9a3dded320f7525d3b66eb7d760d771d5af
sniper23403 <100159541+sniper23403@users.noreply.github.com>
2023-08-23 15:45:06 +0100

    define multitype croos adress

--- index.js
+++ index.js
@@ -8,6 +8,7 @@
 
 const express = require("express");
 const path = require("path");
+const cors = require("cors");
 
 
 
@@ -26,6 +27,11 @@ const port = process.env.PORT || "8000";
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use( 
+  cors({ 
+    origin: "https://sniper23403.github.io/challenge-3/",
+}))
 
 
 
